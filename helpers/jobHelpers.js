8bcd222809b001cdc898bf02357b7ec8e13fdd9b
Bryn McIvor <brynmcivor1@gmail.com>
2022-05-22 14:31:37 +1200

    Add comment to jobHelpers

--- helpers/jobHelpers.js
+++ helpers/jobHelpers.js
@@ -4,6 +4,7 @@ const Job = require('../models/Job');
 const getNextJobNumber = async () => {
   const newestJob = await Job.find().sort({ job_num: -1 }).limit(1);
 
+  // if no jobs exist in DB, start at 1. Else, increment by 1.
   if (newestJob.length < 1) {
     return 1;
   } else {
